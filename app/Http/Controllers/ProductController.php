c2d47212ae83c0d00e4b67b1a46aa354af61ee41
Hamza <hamza.207029@gmail.com>
2023-07-19 04:39:08 -0700

    Add to cart on detail page done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\cart;
 use App\Models\product;
 use Illuminate\Http\Request;
 
@@ -19,5 +20,17 @@ class ProductController extends Controller
     function  search(Request $req){
       $data= product::where("name",'like','%'.$req->Search.'%')->get();
           return View('search',['products'=>$data]);    ;
+    }
+    function addtocart(Request $req){
+     if(session()->has('user')){
+        $user=$req->session()->get('user');
+        $Cart=new cart;
+        $Cart->product_id=$req->product_id;
+        $Cart->user_id=session()->get('user')['id'];
+        $Cart->save();
+        return redirect('/');
+     }else{
+       return redirect('/login');
+     }
     }
 }
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -20,7 +20,11 @@
             <p> {{ $products['description'] }}</p>
             </p>
             <h5>Category: {{ $products['category'] }}</h5>
-            <button class="btn btn-warning">Add to Cart</button>
+            <form action="/add_to_cart" method="POST">
+                @csrf
+                <input type="hidden" name="product_id" value="{{$products['id']}}">
+            <button class="btn btn-warning" type="submit">Add to Cart</button>
+        </form>
             <br>
             <br>
             <button class="btn btn-success">Buy Now</button>
